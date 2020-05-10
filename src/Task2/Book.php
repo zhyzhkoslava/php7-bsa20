f1eef6742bf81addadf6c9491129a2d1f2410aac
Viacheslav <zhizhkoslava@gmail.com>
2020-05-10 23:22:51 +0300

    apdate book.php booksGenerator.php, create index.php for Task 2

--- src/Task2/Book.php
+++ src/Task2/Book.php
@@ -3,19 +3,48 @@
 declare(strict_types=1);
 
 namespace App\Task2;
-
+/**
+ * Class Book
+ * @package App\Task2
+ */
 class Book
 {
+    /**
+     * Book constructor.
+     * @param string $title
+     * @param int $price
+     * @param int $pagesNumber
+     */
+    public function __construct(
+        string $title,
+        int $price,
+        int $pagesNumber
+    )
+    {
+        $this->title = $title;
+        $this->price = $price;
+        $this->pagesNumber = $pagesNumber;
+    }
+
+    /** Get title of the book
+     * @return string
+     */
     public function getTitle(): string
     {
         return $this->title;
     }
 
+    /** Get ptice of the book
+     * @return int
+     */
     public function getPrice(): int
     {
         return $this->price;
     }
 
+    /** Get getPagesNumber of the book
+     * @return int
+     */
     public function getPagesNumber(): int
     {
         return $this->pagesNumber;

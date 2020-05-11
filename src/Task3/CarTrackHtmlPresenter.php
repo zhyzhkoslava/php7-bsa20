48c1fbf2a4ee06c1caef94fc73f1037a20ce8dec
Viacheslav <zhizhkoslava@gmail.com>
2020-05-11 15:59:55 +0300

    task 3

--- src/Task3/CarTrackHtmlPresenter.php
+++ src/Task3/CarTrackHtmlPresenter.php
@@ -7,10 +7,33 @@ namespace App\Task3;
 use App\Task1\Car;
 use App\Task1\Track;
 
+/**
+ * Class CarTrackHtmlPresenter
+ * @package App\Task3
+ */
 class CarTrackHtmlPresenter
 {
+    /**
+     * @param Track $track
+     * @return string
+     */
     public function present(Track $track): string
     {
-        //@todo
+        $carsOnTrack = $track->all();
+        $render = "<div style=text-align:center>";
+        foreach ($carsOnTrack as $car) {
+            $render .= '<div>';
+            $render .= '<h2>Car name - ' . $car->getName() . '</h2>';
+            $render .= '<img src="' . $car->getImage() . '">';
+            $render .= '<p>Speed -  ' . $car->getSpeed() . '</p>';
+            $render .= '<p>Pit Stop Time -  ' . $car->getPitStopTime() . '</p>';
+            $render .= '<p>Fuel Consumption -  ' . $car->getFuelConsumption() . '</p>';
+            $render .= '<p>Fuel Tank Volume -  ' . $car->getFuelTankVolume() . '</p>';
+            $render .= '<hr>';
+            $render .= '</div>';
+        }
+        $render .= '</div>';
+
+        return $render;
     }
 }
